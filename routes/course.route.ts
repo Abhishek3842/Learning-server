a221c7a5e4aeec54484f439c0551f55becbb779d
Abhishek3842 <abhishekcn203@gmail.com>
2024-12-14 10:46:39 +0530

    Initial commit

---
+++ routes/course.route.ts
@@ -0,0 +1,75 @@
+import express from "express";
+import {
+  addAnwser,
+  addQuestion,
+  addReplyToReview,
+  addReview,
+  askAI,
+  deleteCourse,
+  editCourse,
+  generateCertificate,
+  generateVideoUrl,
+  getAdminAllCourses,
+  getAllCourses,
+  getCourseByUser,
+  getSingleCourse,
+  uploadCourse,
+} from "../controllers/course.controller";
+import { authorizeRoles, isAutheticated } from "../middleware/auth";
+import { updateAccessToken } from "../controllers/user.controller";
+const courseRouter = express.Router();
+
+courseRouter.post(
+  "/create-course",
+  isAutheticated,
+  authorizeRoles("admin"),
+  uploadCourse
+);
+
+courseRouter.put(
+  "/edit-course/:id",
+  isAutheticated,
+  authorizeRoles("admin"),
+  editCourse
+);
+
+courseRouter.get("/get-course/:id", getSingleCourse);
+
+courseRouter.get("/get-courses", getAllCourses);
+
+courseRouter.get(
+  "/get-admin-courses",
+  isAutheticated,
+  authorizeRoles("admin"),
+  getAdminAllCourses
+);
+
+courseRouter.get("/get-course-content/:id", isAutheticated, getCourseByUser);
+
+courseRouter.put("/add-question", isAutheticated, addQuestion);
+
+courseRouter.put("/add-answer", isAutheticated, addAnwser);
+
+courseRouter.put("/add-review/:id", isAutheticated, addReview);
+
+courseRouter.put(
+  "/add-reply",
+  isAutheticated,
+  authorizeRoles("admin"),
+  addReplyToReview
+);
+
+courseRouter.post('/getVdoCipherOTP',generateVideoUrl);
+
+courseRouter.delete(
+  "/delete-course/:id",
+  isAutheticated,
+  authorizeRoles("admin"),
+  deleteCourse
+);
+
+courseRouter.post('/askai',updateAccessToken,isAutheticated,askAI);
+
+export default courseRouter;
+
+courseRouter.post('/generate-certificate/:courseId',updateAccessToken, isAutheticated, generateCertificate);
